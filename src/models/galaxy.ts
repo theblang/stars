833f8366330d6fc9e94ab4171bf691c61eccf1c4
Matt Langston <theblang@gmail.com>
2018-05-20 12:46:53 -0500

    Add rendering of a galaxy scene. Tweak tslint to not complain about ordering of properties.

--- src/models/galaxy.ts
+++ src/models/galaxy.ts
@@ -15,35 +15,30 @@ export class Galaxy {
         // This creates and positions a free camera (non-mesh)
         const camera = new BABYLON.FreeCamera(
             'camera1',
-            new BABYLON.Vector3(0, 5, -10),
+            new BABYLON.Vector3(0, 1000, 2000),
             scene
         );
 
-        // this function is the best
         // This targets the camera to scene origin
         camera.setTarget(BABYLON.Vector3.Zero());
 
         // This attaches the camera to the canvas
         camera.attachControl(canvas, true);
 
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        const light = new BABYLON.HemisphericLight(
-            'light1',
-            new BABYLON.Vector3(0, 1, 0),
-            scene
-        );
+        // // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+        // const light = new BABYLON.HemisphericLight(
+        //     'light1',
+        //     new BABYLON.Vector3(0, 1, 0),
+        //     scene
+        // );
 
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7;
+        // // Default intensity is 1. Let's dim the light a small amount
+        // light.intensity = 0.7;
 
-        // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-        const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-
-        // Move the sphere upward 1/2 its height
-        sphere.position.y = 1;
-
-        // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-        BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+        // Draw systems
+        for (const system of this.systems) {
+            system.draw(scene);
+        }
 
         engine.runRenderLoop(() => {
             if (scene) {
